066e8098e1711755a94c781d805102d55a74983a
Zaid <zaidgonzalezbaez@gmail.com>
2025-10-02 09:49:47 -0600

    Instalacion y Configuracion de Modulos para Crear App Hotel WWE

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center h-full'>
+        <h1 className='text-9xl text-red-800'>Hola Mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
