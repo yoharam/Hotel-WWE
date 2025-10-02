f22ea906c26b9fc0b1b74d6d4f71876461797114
Zaid <zaidgonzalezbaez@gmail.com>
2025-10-02 16:37:41 -0600

    Corregir Rutas

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Header from '../assets/components/Header';
+
+
+function Home() {
+  return (
+    <>
+      <div className='flex justify-start items-start h-full'>
+        <Header></Header>
+      </div>
+    </>
+  );
+}
+
+export default Home
