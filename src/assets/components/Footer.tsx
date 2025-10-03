57cdebfa922b095f8da9b4e34b3dddcde3757c66
Zaid <zaidgonzalezbaez@gmail.com>
2025-10-03 10:18:56 -0600

    Nada

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -38,7 +38,7 @@ fill={"currentColor"} viewBox="0 0 24 24">
               <div className="flex items-start space-x-3">
                 <div className="w-2 h-2 bg-red-800 rounded-full mt-2 flex-shrink-0"></div>
                 <div>
-                  <h4 className="font-bold text-red-600">Ring de Luchas</h4>
+                  <h4 className="font-bold text-red-600">Ring de Lucha</h4>
                   <p className="text-sm text-gray-400">Acceso 24/7</p>
                 </div>
               </div>
